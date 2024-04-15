e9a57fe1d6e54d175b8306503acf0a0e656e0332
Frederik Dudzik <5946811+doodzik@users.noreply.github.com>
2024-04-15 11:48:11 -0700

    follow k8s constraint

--- templates/jupyter-pod/main.tf
+++ templates/jupyter-pod/main.tf
@@ -229,7 +229,7 @@ resource "coder_app" "jupyter" {
 resource "kubernetes_pod" "main" {
   count = data.coder_workspace.me.start_count
   metadata {
-    name      = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}"
+    name      = "coder-${lower(data.coder_workspace.me.owner)}-${lower(data.coder_workspace.me.name)}"
     namespace = "coder"
   }
   spec {
@@ -275,7 +275,7 @@ resource "kubernetes_pod" "main" {
 
 resource "kubernetes_persistent_volume_claim" "home-directory" {
   metadata {
-    name      = "home-coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}"
+    name      = "home-coder-${lower(data.coder_workspace.me.owner)}-${lower(data.coder_workspace.me.name)}"
     namespace = "coder"
   }
   wait_until_bound = false
